a462307b765c358f6c10f8f127115326e476039a
Delisa Mason <iskanamagus@gmail.com>
2017-08-24 17:17:52 -0700

    Update examples to reflect new default config

--- examples/koa/bugsnag.js
+++ examples/koa/bugsnag.js
@@ -4,9 +4,7 @@ const unhandledRejection = require("unhandled-rejection");
 // Here we register the Bugsnag client using our project's API key, and optionally pass our
 // Bugsnag configuration. For example, we can enable `sendCode` so we're able to see the code
 // within our app straight from the Bugsnag error report page.
-bugsnag.register("API-KEY-GOES-HERE", {
-  sendCode: true,
-});
+bugsnag.register("API-KEY-GOES-HERE");
 
 // When it comes to unhandled rejections (from Promises) within our app, we can use a library
 // called "unhandled-rejection" to do all the heavy lifting. This library gives us the ability to
