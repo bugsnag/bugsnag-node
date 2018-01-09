a5020641b2b84d11169ec7972a25437d6d9d19b7
Ben Gourley <bn@grly.me>
2018-01-09 14:03:48 +0000

    Refine session tracking
    - migrate option name `sessionTrackingEnabled`->`autoCaptureSessions`
     for consistency with other notifiers
    - session reporting now respects the `notifyReleaseStages` and will
     only send if error reports will

--- test/sessions/delegate.js
+++ test/sessions/delegate.js
@@ -1,11 +1,10 @@
 'use strict'
 
 const proxyquire = require('proxyquire').noCallThru().noPreserveCache()
-
 const Emitter = require('events')
 
 describe('session delegate', () => {
-  it('should', done => {
+  it('should send the session report', done => {
     class TrackerMock extends Emitter {
       start () {
         this.emit('summary', [
@@ -25,8 +24,40 @@ describe('session delegate', () => {
       }
     })
     createSessionDelegate({
-      logger: { info: () => {} },
-      sessionEndpoint: 'blah'
+      logger: { info: () => {}, warn: () => {} },
+      sessionEndpoint: 'blah',
+      notifyReleaseStages: null
+    }).startSession({})
+  })
+
+  it('should not send the session report when releaseStage is not in notifyReleaseStages', done => {
+    class TrackerMock extends Emitter {
+      start () {
+        this.emit('summary', [
+          { startedAt: '2017-12-12T13:54:00.000Z', sessionsStarted: 123 }
+        ])
+      }
+      stop () {}
+      track () {}
+    }
+    const createSessionDelegate = proxyquire('../../lib/sessions', {
+      './tracker': TrackerMock,
+      'request': (opts) => {
+        true.should.equal(false)
+        done()
+      }
+    })
+    createSessionDelegate({
+      logger: {
+        info: () => {},
+        warn: (msg) => {
+          msg.should.equal('Current release stage prevented session report from being sent.')
+          setTimeout(done, 150)
+        }
+      },
+      sessionEndpoint: 'blah',
+      releaseStage: 'qa',
+      notifyReleaseStages: [ 'production' ]
     }).startSession({})
   })
 })
